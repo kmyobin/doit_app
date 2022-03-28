1a68ca2122be856326621890f58724d6241eb4af
kmyobin <sowhat0516@gmail.com>
2022-03-29 02:51:52 +0900

    2022/03/29

---
+++ AndroidLab/app/ch10_notification/src/main/java/org/techtown/ch10_notification/ReplyReceiver.kt
@@ -0,0 +1,25 @@
+package org.techtown.ch10_notification
+
+import android.app.NotificationManager
+import android.app.RemoteInput
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class ReplyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // 알림의 입력 글 획득
+        val replyTxt= RemoteInput.getResultsFromIntent(intent)
+            ?.getCharSequence("key_text-reply")
+
+        Log.d("kkang", "replayTxt : $replyTxt")
+
+        // 알림 취소
+        val manager=context.getSystemService(
+            AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+        manager.cancel(11)
+    }
+}
